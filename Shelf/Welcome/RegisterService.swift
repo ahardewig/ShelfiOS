8153e88891afbc55f4bac4a52fb0e53c29e60168
Alex Hardewig <ahardewig@gmail.com>
2019-10-28 22:38:44 -0400

    Added ability to verify account

--- Shelf/Welcome/RegisterService.swift
+++ Shelf/Welcome/RegisterService.swift
@@ -11,26 +11,54 @@ import Alamofire
 import SwiftyJSON
 
 
+func verifyEmail(email: String, verificationNum: String) {
+    
+    let body: [String: String] = [
+            "email": email,
+            "verificationNum": verificationNum,
+        ]
+    AF.request(DOMAIN + "user/verify-email", method: .post, parameters: body, encoder: JSONParameterEncoder.default).responseJSON { response in
+           
+            if response.response?.statusCode == 200 {
+                print("Success with JSON: \(String(describing: response.data))")
+                ErrorHandler.errorHandler.errorMessageText = "Success!"
+                ErrorHandler.errorHandler.errorDetected = true
+            }
+            else {
+                let error = JSON(response.data as Any)
+                let errorMessage = error["message"].string
+               
+                ErrorHandler.errorHandler.errorMessageText = errorMessage!
+                ErrorHandler.errorHandler.errorDetected = true
+                        
+            }
+            
+        }
+    }
+    
+
 
 func registerUser(username: String, password: String, confirmPassword: String, email: String, birthday: Date) {
-      
+    print (birthday)
+    
     if (!usernameIsValid(username: username)) {return}
     if (!passwordIsValid(password: password, confirmPassword: confirmPassword)) {return}
     if (!emailIsValid(email: email)) {return}
-        
+
     print("USER: " + username);
-        print("PASS: " + password);
-        print("EMAIL: " + email);
+    print("PASS: " + password);
+    print("EMAIL: " + email);
         
-        let dateFormatter = DateFormatter()
-        dateFormatter.dateFormat = "yyyy-MM-dd"
-        let dateString = dateFormatter.string(from: birthday)
-        print("BIRTHDAY: " + dateString);
+//        let dateFormatter = DateFormatter()
+//        dateFormatter.dateFormat = "yyyy-MM-dd"
+//        let dateString = dateFormatter.string(from: birthday)
+//        print("BIRTHDAY: " + dateString);
+
         let body: [String: String] = [
             "username": username,
             "password": password,
             "email": email,
-            "birthday": dateString
+            "birthday": "1990-04-04"
         ]
     AF.request(DOMAIN + "user/register", method: .post, parameters: body, encoder: JSONParameterEncoder.default).responseJSON { response in
            
